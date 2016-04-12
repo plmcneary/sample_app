6486a34cb7b365edfcffa705dc2f287ea66e9ebf
Peter McNeary <plmcneary@icloud.com>
2016-04-11 21:42:23 -0400

    Add integration test for password resets

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ User.create!(name:  "Example User",
              password_confirmation: "foobar",
              admin: true,
              activated: true,
-             activated_at: Time.zone.now))
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -15,5 +15,5 @@ User.create!(name:  "Example User",
                password:              password,
                password_confirmation: password,
                activated: true,
-               activated_at: Time.zone.now))
+               activated_at: Time.zone.now)
 end
